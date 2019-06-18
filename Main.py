aa49e4116ce7e8c901b871fc845e57b1855d6f1e
PythonBotz <46303587+PythonBotz@users.noreply.github.com>
2019-06-18 13:48:44 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,11 @@
 import discord
 from discord.ext import commands
+import os
+
+@bot.event
+async def on_ready():
+  print("Logged in as")
+  print(bot.user.name)
+  print(bot.user.id)
+  
+bot.login(os.
